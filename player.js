778ca98a2f0b0e3635bbe263243dee2b88a0f8de
maximilianjaramazovic <maxi.jaramazovic@gmail.com>
2024-01-04 22:23:21 -0500

    search bar added, only works with actual link

--- player.js
+++ player.js
@@ -1,25 +1,31 @@
 /* HTML Elements */
-const audio_ = document.querySelector('#stream')
+const audio_ = document.getElementById('stream') // audio object
 const play_pause_button_ = document.querySelector('[name="play-pause"]')
 //const play_pause_button_icon_ = play_pause_button_.querySelector('i.fas')
 const volume_control_ = document.querySelector('[name="volume"]')
 const currently_playing_ = document.querySelector('.currently-playing-title')
 const volume_button_ = document.querySelector('[name="mute"]')
 //const volume_button_icon_ = volume_button_.querySelector('i.fas')
+const search_bar_ = document.getElementById("source")
+const search_button_ = document.getElementById("source-send")
 
 let is_playing = false
 let fetch_interval = null
 let current_volume = 0.2
+let source = ""
 
 audio_.volume = current_volume
+//audio_.src = source
+console.log(audio_.src)
+
 
 /**
  * Fetches the currently playing
  * @returns {Promise<any>}
  */
-const fetchCurrentlyPlaying = () => fetch("D:\Files\Audio\Music\The Deli - 532PM.mp3")
+const fetchCurrentlyPlaying = () => fetch(source)
 .then(response => response.json())
-.then(data =>currently_playing_.innerText = data.currentSong)
+.then(data => currently_playing_.innerText = data.currentSong)
 
 //missing icon update function
 /**
@@ -37,13 +43,13 @@ volume_control_.addEventListener('input', () => {
 
 volume_button_.addEventListener('click', () => {
   if (audio_.volume > 0){
-  //adjustVolumeIcon(0)
-  audio_.volume = 0
-  volume_control_.value = 0
+    //adjustVolumeIcon(0)
+    audio_.volume = 0
+    volume_control_.value = 0
   } else {
-  //adjustVolumeIcon(currentVolume)
-  audio_.volume = current_volume
-  volume_control_.value = current_volume
+    //adjustVolumeIcon(currentVolume)
+    audio_.volume = current_volume
+    volume_control_.value = current_volume
   }
 })
 
@@ -51,6 +57,7 @@ play_pause_button_.addEventListener('click', () => {
   if (is_playing)
   {
     audio_.pause()
+    //console.log('paused')
 
     //playPauseButtonIcon.classList.remove('fa-pause')
     //playPauseButtonIcon.classList.add('fa-play')
@@ -59,6 +66,7 @@ play_pause_button_.addEventListener('click', () => {
     currently_playing_.innerText = 'Listen to Some Radio Station'
   } else {
     audio_.play()
+    //console.log('play')
 
     //playPauseButtonIcon.classList.remove('fa-play')
     //playPauseButtonIcon.classList.add('fa-pause')
@@ -67,4 +75,22 @@ play_pause_button_.addEventListener('click', () => {
     fetch_interval = setInterval(fetchCurrentlyPlaying, 3000)
   }
   is_playing = !is_playing
+})
+
+search_button_.addEventListener('click', () => {
+  if (is_playing)
+  {
+    audio_.pause()
+    is_playing = false
+    console.log("radio stopped")
+  }
+  source = search_bar_.value //get value
+  audio_.src = source //set
+  search_bar_.value = "" //reset textbox
+  console.log("source set")
+
+  audio_.load() //reload the player
+  audio_.play() // play
+  is_playing = true // set to play
+  console.log(playing)
 })
